aa5257b51872f9ffcead0d872dc348ca753bfb3a
Daniil <bd3396121@gmail.com>
2021-05-05 19:07:35 +0300

    [ADD]: conversion function to u32 with the specified precision, splitting integer division functions

--- dsky-utils/src/lib.rs
+++ dsky-utils/src/lib.rs
@@ -3,17 +3,34 @@
 use substrate_fixed::types::I9F23;
 
 pub trait MathUtils<RHS = Self> {
-    fn integer_divide(self, rhs: RHS) -> u16;
     fn abs(self) -> Self;
+    fn integer_division_u16(self, rhs: RHS) -> u16;
+    fn integer_division_u32(self, rhs: RHS) -> u32;
+    fn to_u32(self) -> u32;
 }
 
 // Want to change Coord type => impl trait for it here
 impl MathUtils for I9F23 {
-    fn integer_divide(self, rhs: I9F23) -> u16 {
+    fn abs(self) -> Self {
+        self.abs()
+    }
+
+    fn integer_division_u16(self, rhs: I9F23) -> u16 {
         (self / rhs).to_num::<u16>()
     }
 
-    fn abs(self) -> Self {
-        self.abs()
+    fn integer_division_u32(self, rhs: I9F23) -> u32 {
+        (self / rhs).to_num::<u32>()
+    }
+
+    // TODO make this function universal
+    // TODO fix situation when int part consists of 1 number
+    // '100' is the precision for the first two numbers of the fractional part
+    fn to_u32(self) -> u32 {
+        let result: u32 = self.to_num::<u32>() * 100;
+        let first_number_from_frac: u32 = ((self - I9F23::from_num(self.to_num::<u32>())) * 10).to_num::<u32>() * 10;
+        let second_number_from_frac: u32 = ((self - I9F23::from_num(self.to_num::<u32>())) * 100).to_num::<u32>() % 10;
+
+        result + first_number_from_frac + second_number_from_frac
     }
 }
