b82fd001c6be5a6fb63bc34b99018dacacdfa16f
Daniil <bd3396121@gmail.com>
2021-07-06 18:11:06 +0300

    [FIX]: margins

--- pallets/ds-accounts/src/mock.rs
+++ pallets/ds-accounts/src/mock.rs
@@ -25,10 +25,10 @@ construct_runtime!(
 		NodeBlock = Block,
 		UncheckedExtrinsic = UncheckedExtrinsic,
 	{
-		System: frame_system::{Module, Call, Config, Storage, Event<T>},
+        System: frame_system::{Module, Call, Config, Storage, Event<T>},
         Timestamp: pallet_timestamp::{Module, Call, Storage},
         Balances: pallet_balances::{Module, Call, Storage, Config<T>, Event<T>},
-		DSAccountsModule: pallet_ds_accounts::{Module, Call, Storage, Event<T>},
+        DSAccountsModule: pallet_ds_accounts::{Module, Call, Storage, Event<T>},
 	}
 );
 
